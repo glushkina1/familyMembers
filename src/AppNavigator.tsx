e441c26cb07645e13926c48abb067d8ecacbed07
glushkina1 <40953265+glushkina1@users.noreply.github.com>
2021-09-07 12:31:03 +0300

    add linking to web version

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -8,22 +8,26 @@ import {store} from "./store";
 
 const Stack = createStackNavigator();
 
-const MemberNavigator = () => {
-    return (
-        <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="My family" component={HomeScreen}
-                          options={{title: 'Your family members', headerTitleAlign: 'center'}}/>
-            <Stack.Screen name="New Member" component={NewMemberScreen}
-                          options={{title: 'Add a new member to your family', headerTitleAlign: 'center'}}/>
-        </Stack.Navigator>
-    );
-}
+const linking = {
+    prefixes: ['http://localhost:19006/'],
+    config: {
+        screens: {
+            HomeScreen: '',
+            NewMemberScreen: 'newMember',
+        }
+    },
+};
 
 const AppNavigator = () => {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <MemberNavigator/>
+            <NavigationContainer linking={linking}>
+                <Stack.Navigator initialRouteName="HomeScreen">
+                    <Stack.Screen name="HomeScreen" component={HomeScreen}
+                                  options={{title: 'Your family members', headerTitleAlign: 'center'}}/>
+                    <Stack.Screen name="NewMemberScreen" component={NewMemberScreen}
+                                  options={{title: 'Add a new member to your family', headerTitleAlign: 'center'}}/>
+                </Stack.Navigator>
             </NavigationContainer>
         </Provider>
     );
